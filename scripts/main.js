4db5f3558baa1ffa8f057860a33a0b6e44edabda
Diego Melo <diego.nac@hotmail.com>
2025-07-06 18:52:02 -0300

    remove crash code

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,3 @@
 // scripts/main.js
 import initRaceChart from "./race_chart.js";
-import initBarsChart from "./bars_chart.js";
 initRaceChart()
-  .then((ctx) => initBarsChart(ctx))
-  .catch(console.error);
