652ba1a97ee094ed0d72bb3b3432587c8654bc5f
marcio <marciorenan321@gmail.com>
2025-07-06 18:45:48 -0300

    Added transitions for the visualization

--- script.js
+++ script.js
@@ -33,9 +33,46 @@ let nodes = filldata().map((d, i) => {
   };
 });
 
-function getGroupCenter(group) {
+const raceData = [
+  {
+    label: "Parda",
+    percentile: 45,
+    color: "brown",
+  },
+  {
+    label: "Branca",
+    percentile: 43,
+    color: "white",
+  },
+  {
+    label: "Preta",
+    percentile: 10,
+    color: "black",
+  },
+  {
+    label: "Amarela",
+    percentile: 1,
+    color: "yellow",
+  },
+  {
+    label: "Indígena",
+    percentile: 1,
+    color: "red",
+  },
+];
 
-}
+const genderData = [
+  {
+    label: "Homens",
+    percentile: 48,
+    color: "blue",
+  },
+  {
+    label: "Mulheres",
+    percentile: 52,
+    color: "pink",
+  },
+];
 
 const simulation = d3
   .forceSimulation(nodes)
@@ -46,27 +83,72 @@ const simulation = d3
   )
   // we can blind align the y axis to 0 so that things are single file
   .force("y", d3.forceY(height * 0.5).strength(0.05))
+  .alphaTarget(0.3)
   .on("tick", ticked);
 
 // Initialize the nodes positions
-for (const node of nodes) { 
+for (const node of nodes) {
   node.x = node.x * 5 + (width * 0.1 + node.group * width * 0.2);
   node.y = node.y * 5 + center[1];
 }
 
+function Update(data) {
+  let index = 1;
+  let group = 0;
+  for (const node of nodes) {
+    if (index > data[group].percentile) {
+      group++;
+      index = 1;
+    }
+    node.group = group;
+
+    index++;
+  }
+
+  let padding = width * 0.1;
+  let space = (width * 0.8) / (group + 1);
+
+  const t = d3.transition().duration(500).ease(d3.easePolyOut);
+  nodesRender.transition(t).style("fill", (d) => data[d.group].color);
+
+  labels = data.flatMap((d) => d.label);
+  svg
+    .selectAll("text")
+    .data(labels)
+    .join(
+      (enter) => enter.append("text").attr("y", height * 0.5 + 100).attr("x", width / 2),
+      (update) => update,
+
+      (exit) => exit.remove()
+    )
+    .text((d) => d)
+    .transition(t)
+    .attr("x", (d, i) => padding + space / 2 + space * i);
+  // console.log(labels);
+  // labelsRender
+  //   .data(labels)
+  //   .join("text")
+  //   .text((d) => d)
+  //   .transition(t)
+  //   .attr("x", (d, i) => padding + space / 2 + space * i);
+
+  simulation
+    .force(
+      "x",
+      d3.forceX((d) => padding + space / 2 + space * d.group).strength(0.05)
+    )
+    .alpha(1)
+    .restart();
+}
+
 // Create the SVG container.
 const svg = d3.select("svg");
-// .create("svg")
-// .attr("width", width)
-// .attr("height", height)
-// .attr("viewBox", [0, 0, width, height])
-// .attr("style", "max-width: 100%; height: auto; background-color: grey;");
 
-const node = svg
+const nodesRender = svg
   .append("g")
   .attr("stroke", "#000")
   .attr("stroke-width", 1.5)
-  .selectAll("circle") 
+  .selectAll("circle")
   .data(nodes)
   .join("circle")
   .attr("r", 6)
@@ -75,25 +157,24 @@ const node = svg
 const labelsRender = svg
   .selectAll("text")
   .data(labels)
-  .enter()
-  .append("text")
+  .join("text")
   .text((d, i) => d)
   .attr("x", (d, i) => width * 0.1 + i * width * 0.2)
   .attr("y", height * 0.5 + 100)
   .attr("text-anchor", "middle");
 
 // // Add a drag behavior.
-node.call(
+nodesRender.call(
   d3.drag().on("start", dragstarted).on("drag", dragged).on("end", dragended)
 );
 
 function ticked() {
-  node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+  nodesRender.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
 }
 
 // Reheat the simulation when drag starts, and fix the subject position.
 function dragstarted(event) {
-  if (!event.active) simulation.alphaTarget(0.5).restart();
+  // if (!event.active) simulation.alphaTarget(0.5).restart();
   event.subject.fx = event.subject.x;
   event.subject.fy = event.subject.y;
 }
@@ -107,7 +188,19 @@ function dragged(event) {
 // Restore the target alpha so the simulation cools after dragging ends.
 // Unfix the subject position now that it’s no longer being dragged.
 function dragended(event) {
-  if (!event.active) simulation.alphaTarget(0);
+  // if (!event.active) simulation.alphaTarget(0);
   event.subject.fx = null;
   event.subject.fy = null;
 }
+
+const waypoint1 = new Waypoint({
+  element: document.getElementById("text-container-1"),
+  handler: (direction) => (direction == "up" ? Update(raceData) : null),
+  offset: "-50%",
+});
+
+const waypoint2 = new Waypoint({
+  element: document.getElementById("text-container-2"),
+  handler: (direction) => (direction == "down" ? Update(genderData) : null),
+  offset: "50%",
+});
