3540e34c84c7ca44cfdd34427133c9e4e40c5d28
Diego Melo <diego.nac@hotmail.com>
2025-07-06 18:51:19 -0300

    refactor code

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
+// scripts/main.js
 import initRaceChart from "./race_chart.js";
 import initBarsChart from "./bars_chart.js";
-
 initRaceChart()
-  .then(ctx => initBarsChart(ctx))   // ctx = { nodes, color, labels, totals }
+  .then((ctx) => initBarsChart(ctx))
   .catch(console.error);
--- scripts/settings.js
+++ scripts/settings.js
@@ -1,39 +1,17 @@
 // scripts/settings.js
-// Centralize aqui valores-padrão que serão reutilizados em todas as visualizações.
-
 const SETTINGS = {
-  dims: {
-    widthFactor: .6,  // % da largura da janela
-    heightFactor: 1    // % da altura da janela
-  },
-
-
-  colorScheme: [
-    "#66c2a5",
-    "#fc8d62",
-    "#8da0cb",
-    "#e78ac3",
-    "#a6d854"
-  ],
-
-
-  node: {
-    radius: 7,
-    stroke: "#fff",
-    strokeWidth: 1
-  },
-
+  dims: { widthFactor: 0.6, heightFactor: 1 },
+  colorScheme: ["#66c2a5", "#fc8d62", "#8da0cb", "#e78ac3", "#a6d854"],
+  node: { radius: 7, stroke: "#fff", strokeWidth: 1 },
   forces: {
     collideRadius: 7,
     collideStrength: 1,
     xStrength: 0.05,
     yStrength: 0.05,
-    alphaDecay: 0.02,          // taxa de decaimento mais alta → converge mais rápido
-    velocityDecay: 0.35,        // reduz a inércia dos nós
-    dragAlphaTarget: 2       // “reaquece” na hora do drag
+    alphaDecay: 0.02,
+    velocityDecay: 0.35,
+    dragAlphaTarget: 2,
   },
-
-  labelYOffset: 100
+  labelYOffset: 100,
 };
-
 export default SETTINGS;
