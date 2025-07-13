b516310fcc80981a0ab8298382f3c67e65c87688
marcio <marciorenan321@gmail.com>
2025-07-13 18:21:19 -0300

    feature/Add literacy by age

--- scripts/settings.js
+++ scripts/settings.js
@@ -4,6 +4,14 @@ const SETTINGS = {
     ageLabels: ["0-14", "15-29", "30-49", "50-69", "70+"],
     ageColors: ["#a1d99b", "#74c476", "#41ab5d", "#238b45", "#005a32"],
     literacyColors: ["#5ab4ac", "#a9a9a9"],
+    literacyByAge: {
+        labels: ["15-29\nAlfabetizados", "30-49\nAlfabetizados", "50-69\nAlfabetizados", "70+\nAlfabetizados"],
+        colors: {
+            alphabetized: "#5ab4ac",
+            notAlphabetized: "#a9a9a9"
+        },
+        percentages: [98, 95, 87, 68]
+    },
     node: {
         radius: 8,
         stroke: "rgba(255,255,255,0.8)",
